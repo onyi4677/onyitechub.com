2bba094de9244ebd2502cb7dfd66a547edd40305
Onyemaechi Wisdom <onyi4677@gmail.com>
2026-09-22 01:35:58 +0100

    Add Terms of Service footer link

--- app/components/SiteFooter.tsx
+++ app/components/SiteFooter.tsx
@@ -8,7 +8,7 @@ export default function SiteFooter() {
         </div>
         <div>
           <p>© {new Date().getFullYear()} Onyitech JournalHub Ltd</p>
-          <a href="/privacy">Privacy Policy</a>
+          <a href="/privacy">Privacy Policy</a><br /><a href="/terms">Terms of Service</a>
         </div>
       </div>
     </footer>
